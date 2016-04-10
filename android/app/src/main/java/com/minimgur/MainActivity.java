ba81cd85a3aae4c6310f5e10b4a2bc3158728a59
Aysh Su <hevark@msn.com>
2016-04-10 22:02:21 +0800

    (feat) Single image upload

--- android/app/src/main/java/com/minimgur/MainActivity.java
+++ android/app/src/main/java/com/minimgur/MainActivity.java
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+import com.imagepicker.ImagePickerPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -34,7 +37,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new ImagePickerPackage(),
+            new ReactMaterialKitPackage()
         );
     }
 }
--- index.android.js
+++ index.android.js
@@ -1,51 +1,112 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Clipboard,
+    Component,
+    Navigator,
+    ProgressBarAndroid as ProgressBar,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View
 } from 'react-native';
 
+import { CLIENT_ID } from './imgur.config.js';
+
+import { ImagePickerManager } from 'NativeModules';
+
 class minimgur extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.renderScene = this.renderScene.bind(this);
+        this._setClipboardContent = this._setClipboardContent.bind(this);
+        this.uploadToImgur = this.uploadToImgur.bind(this);
+        this.state = {
+            result: ''
+        };
+    }
+
+    render() {
+        return (
+            <Navigator
+                ref="navigator"
+                initialRoute={{name: 'home'}}
+                renderScene={this.renderScene}
+            />
+        );
+    }
+
+    renderScene(route, navigator) {
+        switch (route.name) {
+            case 'home':
+                return (
+                    <TouchableHighlight style={styles.container} onPress={() => this.showUploader()}>
+                        <View style={styles.container}>
+                            <Text style={styles.callToAction}>
+                                Upload to Imgur!
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+                );
+            case 'uploading':
+                return (
+                    <View style={styles.container}>
+                        <ProgressBar styleAttr="Large" />
+                    </View>
+                );
+            case 'results':
+                return (
+                    <View style={styles.container}>
+                        <Text>Result URL have been copied to the clipboard.</Text>
+                        <Text>{this.state.result}</Text>
+                    </View>
+                );
+        }
+    }
+
+    showUploader() {
+        ImagePickerManager.launchImageLibrary({}, (response)  => {
+            this.uploadToImgur(response);
+        });
+    }
+
+    uploadToImgur(imageObject) {
+        this.refs.navigator.push({name: 'uploading'});
+        const formData = new FormData();
+        formData.append('image', imageObject.data);
+        fetch('https://api.imgur.com/3/image',
+            {
+                method: 'POST',
+                headers: {
+                    Authorization: 'Client-ID ' + CLIENT_ID
+                },
+                body: formData
+            }
+        )
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+            this.setState({
+                result: response.data.link
+            });
+            Clipboard.setString(response.data.link);
+            this.refs.navigator.push({name: 'results'});
+        })
+        .catch((ex) => console.log(ex));
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    callToAction: {
+        fontSize: 48,
+        textAlign: 'center',
+        margin: 10
+    },
 });
 
 AppRegistry.registerComponent('minimgur', () => minimgur);
